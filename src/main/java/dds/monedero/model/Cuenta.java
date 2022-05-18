d5c76610d72114c4034efa3c9ed3fd1d49d80e6a
ucabaleiro <ucabaleiro@frba.utn.edu.ar>
2022-05-18 12:00:11 -0300

    remuevo dead code setMovimientos()

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -10,22 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cuenta {
-
   private double saldo = 0;
   private List<Movimiento> movimientos = new ArrayList<>();
-
   public Cuenta() {
     saldo = 0;
   }
-
   public Cuenta(double montoInicial) {
     saldo = montoInicial;
   }
-
-  public void setMovimientos(List<Movimiento> movimientos) {
-    this.movimientos = movimientos;
-  }
-
   public void poner(double cuanto) {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
@@ -37,7 +29,6 @@ public class Cuenta {
 
     new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
   }
-
   public void sacar(double cuanto) {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
@@ -53,27 +44,22 @@ public class Cuenta {
     }
     new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
   }
-
   public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
     Movimiento movimiento = new Movimiento(fecha, cuanto, esDeposito);
     movimientos.add(movimiento);
   }
-
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
         .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
-
   public List<Movimiento> getMovimientos() {
     return movimientos;
   }
-
   public double getSaldo() {
     return saldo;
   }
-
   public void setSaldo(double saldo) {
     this.saldo = saldo;
   }
